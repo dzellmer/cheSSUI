9699fdb71631460048723f9b06888c2ab00fd39b
dzellmer <dzellmer@andrew.cmu.edu>
2022-12-01 13:06:50 -0500

    Basic movement

--- src/App.js
+++ src/App.js
@@ -4,16 +4,18 @@ import appRoutes from './shared/appRoutes';
 import MainMenu from './containers/MainMenu/MainMenu';
 import GamePage from './containers/GamePage/GamePage'; 
 
+import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 
 function App() {
+  const [position, setPosition] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
           {/* <Route path={appRoutes.menu} element={<MainMenu />}></Route> */}
-          <Route path={appRoutes.game} element={<GamePage />}></Route>
+          <Route path={appRoutes.game} element={<GamePage initPosition={position} />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/containers/GamePage/GamePage.js
+++ src/containers/GamePage/GamePage.js
@@ -1,17 +1,32 @@
+import { Chess } from "chess.js";
 import Chessboard from "chessboardjsx";
 import React, { useState } from "react";
 import { Button } from "reactstrap";
 
 export default function GamePage(props) {
-    const [position, setPosition] = useState("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+    const [chessObj, setChessObj] = useState(new Chess(props.position));
+    const [position, setPosition] = useState(chessObj.fen());
+
+    const makeMove = (move) => {
+        chessObj.move(move);
+        setPosition(chessObj.fen());
+    }
 
     return (
         <div className="GamePage">
-            <Chessboard width={600} position={position}/>
+            <Chessboard 
+                width={600} 
+                position={position}
+                onDrop={(move) => makeMove({ 
+                    from: move.sourceSquare, 
+                    to: move.targetSquare, 
+                    piece: move.piece
+                })}
+            />
             <div className="game-menu">
-                <Button size="lg" disabled>Undo Move</Button>
-                <Button size="lg" disabled>Restart Game</Button>
-                <Button size="lg">Main Menu</Button>
+                <Button className="game-menu-button" color="warning" size="lg" disabled>Undo Move</Button>
+                <Button className="game-menu-button" color="warning" size="lg" disabled>Restart Game</Button>
+                <Button className="game-menu-button" color="warning" size="lg">Main Menu</Button>
             </div>
         </div>
     )
